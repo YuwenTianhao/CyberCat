472739349809926d63cd2e9176ba7d3e2ffe4d19
XuChunhaoBUAA <118552880+XuChunhaoBUAA@users.noreply.github.com>
2022-11-19 20:15:53 +0800

    Update cat.ino
    掉头距离改为23

--- cat/cat.ino
+++ cat/cat.ino
@@ -224,7 +224,7 @@ void setup() {
     while (1)
     {
       double juli = CalculateDistance();
-      if (juli >= 25)
+      if (juli >= 23)
       {
         balance();
         servo[12].write(angleC16);
@@ -239,7 +239,7 @@ void setup() {
           runC();
           runD();
           juli = CalculateDistance();
-          if(juli<=25){
+          if(juli<=23){
             break;
           }
         }
